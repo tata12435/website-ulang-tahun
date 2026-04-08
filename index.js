3d34f9eef19d2b8470cc860b1a13c916c5ad0450
tata12435 <superemeltata@gmail.com>
2026-04-08 22:07:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function validateForm() {
     var password = document.getElementById("password").value;
 
     //validasi username dan password, username dan password bisa diganti sesuai keinginan
-    if (username === "pacar kalian" && password === "pacar kalian") {
+    if (username === "belva imup" && password === "i love u belva") {
         Swal.fire({
             icon: "success",
             title: "Login berhasil!",
-            text: "Selamat datang pacar kaliankk",
+            text: "Selamat datang belva",
             showConfirmButton: false,
             timer: 1500,
         }).then(function () {
